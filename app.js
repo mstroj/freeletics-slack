4e9b8f86414ed5bda0b6da6e6ca4ab1c94c48bc2
Tiziano Rullo <tiziano.rullo@liip.ch>
2015-08-01 16:44:30 +0200

    get feed

--- app.js
+++ app.js
@@ -2,33 +2,40 @@ var request = require("request");
 
 var user = '';
 
-request({
-    url: "https://api.freeletics.com/v2/login.json",
-    method: "POST",
-    json: {
-        'user' : {'email' : 'public.mcs@gmail.com', 'password' : 'test1234' }
-    },
-    timeout: 10000,
-    followRedirect: true,
-    maxRedirects: 10
-}, function(error, response, body) {
-    //console.log(body);
-    user = body.user;
+getUser();
 
+function getUser() {
     request({
-        uri: "https://api.freeletics.com/v2/profile.json",
-        method: "GET",
+        url: 'https://api.freeletics.com/v2/login.json',
+        method: 'POST',
+        json: {
+            'user' : {
+                'email' : 'public.mcs@gmail.com',
+                'password' : 'test1234' 
+            }
+        },
+        timeout: 10000,
+        followRedirect: true,
+        maxRedirects: 10
+    }, setUser);
+};
+
+function setUser(err, res, body) {
+    user = body.user;
+    getFeedWithFollowings();
+};
+
+function getFeedWithFollowings() {
+    request({
+        uri: 'https://api.freeletics.com/v2/users/' + user.id +'/feed_entries/with_followings.json',
+        method: 'GET',
         headers: {
             'Authorization': 'Token token=' + user.auth_token
         },
         timeout: 10000,
         followRedirect: true,
         maxRedirects: 10
-    }, function(error, response, body) {
-        console.log(body);
+    }, function () {
+        console.log(arguments);
     });
-
-});
-
-
-
+};
