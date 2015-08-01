f321e3fc873d6f1734cddee00f4ab05c40f7a1c6
Marlene Stroj <marlene.stroj@gmail.com>
2015-08-01 19:25:35 +0200

    Slack post

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var request = require("request");
 
-var user = '';
-var feedEntries;
+var user = '',
+    feedEntries,
+    lang = 'en';
+
+var freeleticsUrl = 'http://www.freeletics.com/' + lang + '/';
+
 
 getUser();
 
@@ -31,7 +35,8 @@ function getFeedWithFollowings() {
         uri: 'https://api.freeletics.com/v2/users/' + user.id +'/feed_entries/with_followings.json',
         method: 'GET',
         headers: {
-            'Authorization': 'Token token=' + user.auth_token
+            //'Authorization': 'Token token=' + user.auth_token
+            'Authorization': 'Token token=1XbzfzXS5qSNFnhZM1D6Jw'
         },
         timeout: 10000,
         followRedirect: true,
@@ -40,6 +45,52 @@ function getFeedWithFollowings() {
 };
 
 function parseFeed (err, res, body) {
-    feedEntries = JSON.parse(body);
-    console.log(feedEntries);
+    feedEntries = JSON.parse(body).feed_entries;
+    //console.log(feedEntries);
+    postToSlack(feedEntries[0]);
+}
+
+function postToSlack(data) {
+
+    var woUser = data.user,
+        woFeedEntry = data.object,
+        wo = woFeedEntry.workout,
+        woMinutes = Math.floor(woFeedEntry.seconds/60),
+        woSeconds = woFeedEntry.seconds - woMinutes * 60,
+        star = woFeedEntry.star ? ':star:' : '',
+        personalBest = woFeedEntry.personal_best ? ' PB ' : '',
+        postText = woUser.first_name + ' ' + woUser.last_name +
+            ' finished workout ' + wo.title + ' in ' +
+            woMinutes + ':' + woSeconds + '!',
+        postTitle = wo.title + ' ' +
+            star + ' ' + personalBest +
+            woMinutes + ':' + woSeconds ;
+
+    request({
+        url: 'https://hooks.slack.com/services/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',
+        method: 'POST',
+        json: {
+            //'text' : postText ,
+            'attachments' : [{
+                'text' : wo.fitness_variant,
+                'color' : '#000000',
+                'fallback' : postText,
+                'image_url' : woFeedEntry.picture.feed,
+                'author_name' : woUser.first_name + ' ' + woUser.last_name,
+                'author_link' : freeleticsUrl + 'users/' + woUser.id + '/feed',
+                'author_icon' : woUser.profile_pictures.small,
+                'title' : postTitle,
+                'title_link' : freeleticsUrl + 'community/feed-entries/training' + woFeedEntry.id
+            }]
+        },
+        timeout: 10000,
+        followRedirect: true,
+        maxRedirects: 10
+    }, function(err, res, body){
+        //console.log(res);
+        if (err) {
+            console.log(err);
+        }
+    });
+
 }
